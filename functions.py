18a8d920a31c5cbc734de8844ec37dc4c8842131
Rens <rensoostenbach@hotmail.com>
2021-02-22 14:26:32 +0100

    Black - pretty code

--- functions.py
+++ functions.py
@@ -1,11 +1,20 @@
 import numpy as np
-from PIL import  Image
+from PIL import Image
 import cv2
 from math import ceil
 
-COLORS = [(123,140,191), (0, 0, 0), (244,205,83), (0, 0, 255),
-          (128, 169, 128), (170, 175, 140), (205,181,169), (120, 110, 125),
-          (41,43,81), (183, 172, 140)]
+COLORS = [
+    (123, 140, 191),
+    (0, 0, 0),
+    (244, 205, 83),
+    (0, 0, 255),
+    (128, 169, 128),
+    (170, 175, 140),
+    (205, 181, 169),
+    (120, 110, 125),
+    (41, 43, 81),
+    (183, 172, 140),
+]
 
 
 def draw_bbox(img, csv):
@@ -16,13 +25,24 @@ def draw_bbox(img, csv):
         im_arr_bgr = cv2.cvtColor(im_arr, cv2.COLOR_RGB2BGR)
         width = im_arr_bgr.shape[1]
         for index, row in csv.iterrows():
-            cv2.rectangle(im_arr_bgr,
-                          (row.x, row.y+row.Height), (row.x+row.Width, row.y),
-                          color=COLORS[index], thickness=ceil(width/250))
+            # Draw bounding box
+            cv2.rectangle(
+                img=im_arr_bgr,
+                pt1=(row.x, row.y + row.Height),
+                pt2=(row.x + row.Width, row.y),
+                color=COLORS[index],
+                thickness=ceil(width / 250),
+            )
             # Put text on bottom left of the bounding box
-            cv2.putText(img=im_arr_bgr, text=f"{row.Label}: {row.Score}", org=(row.x, row.y+row.Height),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=width/1000,
-                        color=COLORS[index], thickness=ceil(width/500))
+            cv2.putText(
+                img=im_arr_bgr,
+                text=f"{row.Label}: {row.Score}",
+                org=(row.x, row.y + row.Height),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=width / 1000,
+                color=COLORS[index],
+                thickness=ceil(width / 500),
+            )
         im_arr = cv2.cvtColor(im_arr_bgr, cv2.COLOR_BGR2RGB)
         # convert back to Image object
         bbox_image = Image.fromarray(im_arr)
