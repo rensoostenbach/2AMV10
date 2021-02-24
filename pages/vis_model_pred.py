44e59419a67f1418781472289b4e6b95451fc6f5
Rens <rensoostenbach@hotmail.com>
2021-02-24 11:38:26 +0100

    Confidence threshold, choose person and image number

--- pages/vis_model_pred.py
+++ pages/vis_model_pred.py
@@ -3,36 +3,40 @@ import streamlit as st
 import random
 from pathlib import Path
 from functions import draw_bbox
+import glob
 
 
 def write():
-    df = pd.DataFrame({"first column": [1, 2, 3, 4], "second column": [10, 20, 30, 40]})
+    # Let user decide person number
+    person_number = st.sidebar.selectbox("Choose person number (1-40)", list(range(1, 41)))
 
-    st.write(df)
+    # Choose amount of images that user has
+    data_folder = Path("../2AMV10/data/raw/")
+    jpg_counter = len(glob.glob1(data_folder / f"Person{person_number}", "*.jpg"))
+    file_number = st.sidebar.selectbox("Choose image number", list(range(1, jpg_counter+1)))
 
-    option = st.sidebar.selectbox("Which number do you like best?", df["first column"])
+    # Add in confidence slider.
+    confidence_threshold = st.sidebar.slider(
+        'Confidence threshold: What is the minimum acceptable confidence level for displaying a bounding box?', 0.0,
+        1.0, 0.5, 0.01)
+    st.sidebar.text(f"The current threshold value is {confidence_threshold}")
 
-    st.write("You selected:", option)
-
-    st.write("Let's show a random picture and the associated csv file with it.")
-    person_number = random.randint(1, 40)
-    file_number = random.randint(1, 5)
     st.write(
-        f"We are looking at a picture of person nummber {person_number} with image number {file_number}."
+        f"We are looking at a picture of person number {person_number} with image number {file_number}."
     )
 
-    data_folder = Path("../2AMV10/data/raw/")
     img = data_folder / f"Person{person_number}/Person{person_number}_{file_number}.jpg"
 
     predictions = (
         data_folder / f"Person{person_number}/Person{person_number}_{file_number}.csv"
     )
-    csv = pd.read_csv(predictions)
+    df = pd.read_csv(predictions)
+    df = df[df['Score'] >= confidence_threshold]
 
-    image, bbox_image = draw_bbox(img, csv)
+    image, bbox_image = draw_bbox(img, df)
     st.image(image, caption="Random picture", use_column_width=True)
     st.image(
         bbox_image, caption="Random picture with bounding boxes", use_column_width=True
     )
 
-    st.write(csv)
+    st.write(df)
