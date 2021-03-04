a9b89d79fee1f24ebb23005899dbff627aa6c2a0
Rens <rensoostenbach@hotmail.com>
2021-03-04 19:03:00 +0100

    New model - Efficientdet D0 TF2
    Hopefully we can use tf-explain on this

--- pages/vis_model_pred.py
+++ pages/vis_model_pred.py
@@ -1,21 +1,19 @@
 import pandas as pd
 import streamlit as st
-import random
 from pathlib import Path
-from functions import draw_bbox
+from functions import draw_bbox, visualize_hog
 import glob
 
 
 def write():
-    # TODO: Let user choose between visualising training images and real images. For training images, do HOG Features.
+    data_folder = Path("../2AMV10/data/raw/")
 
     # Let user decide person number
     person_number = st.sidebar.selectbox("Choose person number (1-40)", list(range(1, 41)))
 
     # Choose amount of images that user has
-    data_folder = Path("../2AMV10/data/raw/")
     jpg_counter = len(glob.glob1(data_folder / f"Person{person_number}", "*.jpg"))
-    file_number = st.sidebar.selectbox("Choose image number", list(range(1, jpg_counter+1)))
+    file_number = st.sidebar.selectbox("Choose image number", list(range(1, jpg_counter + 1)))
 
     # Add in confidence slider.
     confidence_threshold = st.sidebar.slider(
@@ -30,15 +28,26 @@ def write():
     img = data_folder / f"Person{person_number}/Person{person_number}_{file_number}.jpg"
 
     predictions = (
-        data_folder / f"Person{person_number}/Person{person_number}_{file_number}.csv"
+            data_folder / f"Person{person_number}/Person{person_number}_{file_number}.csv"
     )
     df = pd.read_csv(predictions)
     df = df[df['Score'] >= confidence_threshold]
 
     image, bbox_image = draw_bbox(img, df)
-    st.image(image, caption="Random picture", use_column_width=True)
-    st.image(
-        bbox_image, caption="Random picture with bounding boxes", use_column_width=True
-    )
+
+    hog_image_rescaled = visualize_hog(img, pixels_per_cell=(64, 64))
+
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        st.image(image, caption="Random picture", use_column_width='auto')
+    with col2:
+        st.image(
+            bbox_image, caption="Random picture with bounding boxes", use_column_width='auto'
+        )
+    with col3:
+        st.image(
+            hog_image_rescaled, caption="Histogram of Oriented Gradients (HOG)", use_column_width='auto'
+        )
 
     st.write(df)
+
