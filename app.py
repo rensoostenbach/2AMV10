8fa7dbf27aa0bb4ac6cd9ad2d1df3a86e6f0a0be
Rens <rensoostenbach@hotmail.com>
2021-03-24 14:48:37 +0100

    Remove "Understanding the model"

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import awesome_streamlit as ast
-import pages.vis_model_pred
 import pages.relations_visualizations
 import pages.people
 import pages.objects
@@ -8,7 +7,6 @@ import pages.objects
 st.set_page_config(layout="wide")
 
 PAGES = {
-    "Understanding the model": pages.vis_model_pred,
     "People": pages.people,
     "Objects": pages.objects,
     "People and predicted objects": pages.relations_visualizations,
